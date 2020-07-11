e460e6134f9df3d7730c286d269a23f65da1afa8
ayodeleoniosun <heywire4u@gmail.com>
2020-07-11 12:52:55 +0100

    Set up api routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+	Route::get('/welcome', 'GamesController@welcome');
+        
+    //players routes
+    Route::group(['prefix' => 'players'], function() {
+        Route::post('/register', 'PlayerController@register');
+        Route::get('/', 'PlayersController@players');
+        Route::get('/games/{playerId}', 'GameController@playerGames');
+        Route::get('/played-games/{playerId}', 'GameController@playerplayedGames');
+        Route::get('/top/{month}', 'GameController@topPlayers');
+        Route::get('/top/games/{month}', 'GameController@topPlayersGames');
+    });
+    
+	//games routes
+    Route::group(['prefix' => 'games'], function() {
+        Route::post('/add', 'GameController@add');
+        Route::get('/', 'GameController@games');
+        Route::get('/date/{date}', 'GameController@gamesPerDay');
+        Route::get('/range/{dateRange}', 'GameController@gamesDateRange');
+    });
+    
 });
