76d88e5d78b665387e28ab021303b7f4950e6cc3
ayodeleoniosun <heywire4u@gmail.com>
2020-07-11 15:07:52 +0100

    Fix games resource bug

--- app/Http/Resources/GameResource.php
+++ app/Http/Resources/GameResource.php
@@ -22,7 +22,7 @@ class GameResource extends JsonResource
         return [
             'id' => $this->id,
             'player_id' => $this->player_id,
-            'player_fullname' => ucwords($this->Player->firstname." ".$this->Player->lastname),
+            'player_fullname' => ucwords($this->Players->firstname." ".$this->Players->lastname),
             'name' => $this->name,
             'version' => $this->version,
             'created_at' => Carbon::parse($this->created_at)->format('F jS, Y, h:i: A'),
