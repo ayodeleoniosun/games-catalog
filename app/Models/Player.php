457e4b1f0caf5a683b0bdc0f127135cfd749a3b3
ayodeleoniosun <heywire4u@gmail.com>
2020-07-11 12:27:56 +0100

    Set up players model

---
+++ app/Models/Player.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Player extends  Authenticatable implements JWTSubject
+{
+    use SoftDeletes;
+
+    protected $table = 'players';
+	
+	protected $fillable = [
+    	'firstname', 
+    	'lastname',
+    	'email',
+    	'password',
+    	'last_logged_in'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
+    public function games() {
+        return $this->hasMany(Games::class);
+    }
+
+    public function invitations() {
+        return $this->hasMany(Invitations::class);
+    }
+
+    public function myInvitations() {
+        return $this->hasMany(Invitations::class);
+    }
+
+    public function playedGames() {
+        return $this->hasMany(playedGames::class);
+    }
+
+}
